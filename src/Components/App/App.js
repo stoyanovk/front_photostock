8fc93a07d42f671a944fb20bfdd6a7a27d106943
Kostia <kostia@Mac-mini-Kostia.local>
2019-09-17 17:04:10 +0300

    fast commit

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,18 +1,20 @@
-import React, { createContext } from "react";
+import React, { createContext,useState,useEffect} from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "../../Store/store";
 import Routes from "../../Routes";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import GlobalStyle from "../GlobalStyle";
-const arr =[1,2,3,45,6,7]
 const MyContext = createContext();
+
 const App = () => {
+  
+ 
   return (
     <Provider store={store}>
       <GlobalStyle />
       <CssBaseline />
-      <MyContext.Provider value={arr}>
+      <MyContext.Provider>
         <Router>
           <Routes />
         </Router>
--- src/Pages/Home/SecondSection.js
+++ src/Pages/Home/SecondSection.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useEffect, useState} from "react";
 import { Title, SecondSectionWrapper } from './style';
-import VariableWidthSlider from '../../Components/VariableWidthSlider'
+import VariableWidthSlider from '../../Components/VariableWidthSlider';
+import { photosApi } from "../../Api";
+
 export default function SecondSection() {
+  const [popularImage, setPopularImage] = useState([]);
+  useEffect(() => {
+    photosApi.getPhotos().then(({ data: { data } }) => {
+      setPopularImage(data)
+    })
+  }, [])
+  const sliderItem = popularImage.map(({ date, title, imageUrl, _id }) => {
+    return (
+      <div key={_id}>
+        <img
+          src={`https://photostock-back.herokuapp.com/upload/${imageUrl}`}
+          alt={title}
+        />
+      </div>
+    );
+  });
   return (
       <SecondSectionWrapper>
           <Title>Newest Images</Title>
-          <VariableWidthSlider/>
+      <VariableWidthSlider />
       </SecondSectionWrapper>
   );
 }
+
