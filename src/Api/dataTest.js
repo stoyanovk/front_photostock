ea23272f68774748619058213728a11736ad88f0
Kostia <kostia@Mac-mini-Kostia.local>
2019-09-18 17:01:08 +0300

    made test data

---
+++ src/Api/dataTest.js
@@ -0,0 +1,17 @@
+// const data = {
+//     images: [
+//         {
+//             id: 111
+//   image_url: './image/'
+//   name: varchar
+//   description varchar
+//   user_id int [ref: > users.id]
+//   tags array [ref: > tags.id]
+//     comments array [ref: > comments.id]
+//     downloads int
+//   created_at datetime
+//   updated_at datetime
+//   category_id int [ref: > categories.id]
+// }
+//     ]
+// }
