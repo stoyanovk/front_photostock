a85829a0b4c202050e003f19e385f8d3969e53e5
Kostia <kostia@Mac-mini-Kostia.local>
2019-09-26 16:40:27 +0300

    made linter fix

--- src/Pages/Home/CategorySection.js
+++ src/Pages/Home/CategorySection.js
@@ -1,28 +1,26 @@
-import React from "react";
-import GridList from "@material-ui/core/GridList";
-import GridListTile from "@material-ui/core/GridListTile";
-import GridListTileBar from "@material-ui/core/GridListTileBar";
-import { Title, SectionWrapper } from "./style";
-import { withConsumer } from "../../HOC";
-import { withRouter } from "react-router-dom";
-import {redirectTo} from '../../utils'
+import React from 'react';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import { Title, SectionWrapper } from './style';
+import { withConsumer } from '../../HOC';
+import { withRouter } from 'react-router-dom';
+import { redirectTo } from '../../utils';
+
 function CategorySection({ value: { categories }, history }) {
- 
   return (
     <SectionWrapper>
       <Title>Categories</Title>
       <GridList cellHeight={260} cols={6}>
-        {categories.map(({ id, name, label }) => {
-          return (
+        {categories.map(({ id, name, label }) => (
             <GridListTile
               key={id}
               cols={2}
-              onClick={redirectTo(id, history.push,'category')}>
+              onClick={redirectTo(id, history.push, "category")}>
               <img src={label} alt={name} />
               <GridListTileBar title={name} />
             </GridListTile>
-          );
-        })}
+          ))}
       </GridList>
     </SectionWrapper>
   );
--- src/Pages/PhotoPage/PhotoPage.js
+++ src/Pages/PhotoPage/PhotoPage.js
@@ -1,45 +1,47 @@
-import React, { useEffect, useState } from "react";
-import PageWrapper from "../../Components/PageWrapper";
-import Container from "@material-ui/core/Container";
-import Grid from "@material-ui/core/Grid";
-import PhotoContainer from "./PhotoContainer";
-import PhotoPageSlider from "./PhotoPageSlider";
-import { photosApi } from "../../Api";
+import React, { useEffect, useState } from 'react';
+import PageWrapper from '../../Components/PageWrapper';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import PhotoContainer from './PhotoContainer';
+import PhotoPageSlider from './PhotoPageSlider';
+import { photosApi } from '../../Api';
+import PropTypes from 'prop-types';
 
 function PhotoPage({ id }) {
   const [imagesArr, setImagesArr] = useState([]);
-  const [image, setImage] = useState({});
-  console.log(id);
+  const [stateImage, setImage] = useState({});
+
   let isMount = false;
   useEffect(() => {
     isMount = true;
-    isMount &&
-      photosApi.getOnePhoto(id).then(({ data: { data: { image } } }) =>
-        setImage(image)
-      );
-    // setTimeout(() => {
-    //   if (isMount) {
-    //     setImagesArr(images);
-    //     const image = images.find((image) => image.id === id)
-    //     setImage(image);
-    //   }
+    if (isMount) {
+      photosApi
+        .getOnePhoto(id)
+        .then(({ data: { data: { image } } }) => setImage(image))
+        .catch((err) => new Error(err));
+    }
 
-    // }, 800);
-    return () => (isMount = false);
+    return () => {
+      isMount = false;
+      return isMount;
+    };
   }, []);
   return (
     <PageWrapper>
-      <Container maxWidth='xl'>
+      <Container maxWidth="xl">
         <Grid container>
           <Grid item xs={12} lg={9}>
-            <PhotoContainer image={image} />
+            <PhotoContainer image={stateImage} />
             <PhotoPageSlider images={imagesArr} />
           </Grid>
-          <Grid item xs={12} lg={3}></Grid>
+          <Grid item xs={12} lg={3} />
         </Grid>
       </Container>
     </PageWrapper>
   );
 }
 
+PhotoPage.propTypes = {
+  id: PropTypes.string.isRequired,
+};
 export default PhotoPage;
